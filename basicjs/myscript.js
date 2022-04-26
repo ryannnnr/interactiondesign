1963e69171242ceb0599eea3a1f2e6a986af64d6
ryannnnr <ruskr011@newschool.edu>
2022-04-26 14:44:08 -0400

    yes

--- basicjs/myscript.js
+++ basicjs/myscript.js
@@ -1,23 +1,16 @@
-
-for (var n=0; n <= 20; n = n+1) {
+for (var n=1; n <= 20; n = n+1) {
   if ((n+1)%2==0){
-    console.log(n + " is odd.");
+    document.getElementById("evenodd").innerHTML = document.getElementById("evenodd").innerHTML + n + " is odd.</br> ";
   } else {
-    console.log(n + " is even.");
-  }
   document.getElementById("evenodd").innerHTML =
-      document.getElementById("evenodd").innerHTML + [n] ;
+      document.getElementById("evenodd").innerHTML + n + " is even.</br> ";
 }
-
+};
 
 var cars = ["Dog", "Cat", "Mouse"];
 for ( var i = cars.length-1; i >= 0; i-- ) {
-  // console.log(i);
   console.log(cars[i]);
   console.log(i);
-  // Below I output the data to the webpage. I take the innterHTML of "j0utput"
-  //      and add a space and then the value of cars [i] with every interation of
-  //      the loop.
   document.getElementById("carsbackwards").innerHTML =
     document.getElementById("carsbackwards").innerHTML + " " + cars[i];
 }
@@ -25,25 +18,26 @@ for ( var i = cars.length-1; i >= 0; i-- ) {
 
 var numset1 = [2, 5, 6];
 var numset2 = [4, 9, 3];
-var numset3 = [6, 14, 9];
-console.log( numset[0] + numset[0] );
+var numset3 = [];
+console.log( numset1[0] + numset2[0] );
 for ( y = 0; y < numset1.length; y++) {
-  console.log( numset1[y] + numset[y] );
-  numset3[y] = numset[y] + numset[y];
-
+  console.log( numset1[y] + numset2[y] );
+  document.getElementById("numset").innerHTML = document.getElementById("numset").innerHTML +
+    (numset1[y]+numset2[y]) + ", ";
+  numset3[y] = numset1[y] + numset2[y];
+}
 console.log(numset3);
 
-document.getElementById("numset").innerHTML =
-    document.getElementById("numset").innerHTML + [y] ;
-}
 
-
-let str = "interactions";
-for (let i = 0; i < str.length; i++) {
-  console.log(str[i]);
-  for (let char of "interactions") {
-  console.log(char)
-}
-  document.getElementById("string").innerHTML =
-      document.getElementById("string").innerHTML + (str[i]) + (n);
+let str1 = "interactions";
+var str2 = "";
+console.log(str1);
+console.log(str1.length);
+for ( p = 0; p < str1.length; p++) {
+  if (p%2==0){
+    str2 += "Z";
+} else {
+ str2 += str1[p];
   }
+};
+console.log(str2);
